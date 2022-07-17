bed7dc46c6ff135535bc3c2708c6ff181fcee956
Josh King <skierx@gmail.com>
2022-07-17 11:42:35 -0400

    These components don't appear to be used

--- src/extensions/contact-loaders/actionnetwork/react-component.js
+++ src/extensions/contact-loaders/actionnetwork/react-component.js
@@ -1,16 +1,7 @@
 import type from "prop-types";
 import React from "react";
-import RaisedButton from "material-ui/RaisedButton";
 import GSForm from "../../../components/forms/GSForm";
 import Form from "react-formal";
-import Subheader from "material-ui/Subheader";
-import Divider from "material-ui/Divider";
-import { ListItem, List } from "material-ui/List";
-import CampaignFormSectionHeading from "../../../components/CampaignFormSectionHeading";
-import CheckIcon from "material-ui/svg-icons/action/check-circle";
-import WarningIcon from "material-ui/svg-icons/alert/warning";
-import ErrorIcon from "material-ui/svg-icons/alert/error";
-import { StyleSheet, css } from "aphrodite";
 import yup from "yup";
 
 export class CampaignContactsForm extends React.Component {
